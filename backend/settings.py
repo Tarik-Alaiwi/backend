0f6c71aa8415154cb1a195bb26cf3e69f81979cf
Tarik-Alaiwi <54720296+Tarik-Alaiwi@users.noreply.github.com>
2025-09-26 17:30:26 +0200

    initial

--- backend/settings.py
+++ backend/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -71,6 +73,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "backend.wsgi.application"
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
